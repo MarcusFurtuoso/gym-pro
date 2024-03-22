a65cbf4fdddd135f619ac418a4b0a06ea1201aba
MarcusFurtuoso <marcusvini678@gmail.com>
2024-03-22 10:31:29 -0300

    feat: add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBj__q-NB1PNfZ0-Mp2CVYRLsQXl20khvQ",
+    authDomain: "gym-pro-5996c.firebaseapp.com",
+    projectId: "gym-pro-5996c",
+    storageBucket: "gym-pro-5996c.appspot.com",
+    messagingSenderId: "868475659168",
+    appId: "1:868475659168:web:a28b02d0aa5c62f092bac5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBj__q-NB1PNfZ0-Mp2CVYRLsQXl20khvQ",
+    authDomain: "gym-pro-5996c.firebaseapp.com",
+    projectId: "gym-pro-5996c",
+    storageBucket: "gym-pro-5996c.appspot.com",
+    messagingSenderId: "868475659168",
+    appId: "1:868475659168:web:a28b02d0aa5c62f092bac5"
+  }
 };
 
 /*
