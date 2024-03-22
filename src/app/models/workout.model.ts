d06b7f9dfbdbfbe6ed30406fe73ec6e38595a193
MarcusFurtuoso <marcusvini678@gmail.com>
2024-03-22 10:25:22 -0300

    feat: add models

---
+++ src/app/models/workout.model.ts
@@ -0,0 +1,12 @@
+export interface Workout {
+  id: string;
+  title: string;
+  description: string;
+  datetime: string;
+  exercises: Exercise[];
+ }
+
+ export interface Exercise {
+    name: string;
+    completed: boolean
+ }
