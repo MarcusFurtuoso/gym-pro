958e49efe8a8367c022bc7a6397ca510a4f1e48c
MarcusFurtuoso <marcusvini678@gmail.com>
2024-03-28 14:02:08 -0300

    feat: implements component

--- src/app/shared/components/add-select-workout/add-select-workout.component.ts
+++ src/app/shared/components/add-select-workout/add-select-workout.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
-import { Workout } from 'src/app/models/workout.model';
+import { Exercise, Workout } from 'src/app/models/workout.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
 
@@ -11,11 +11,8 @@ import { UtilsService } from 'src/app/services/utils.service';
   styleUrls: ['./add-select-workout.component.scss'],
 })
 export class AddSelectWorkoutComponent implements OnInit {
-  @Input() workout: any; // os dados do treino
-  @Input() isEditable: boolean; // se o treino é editável
-
+  @Input() workout: Workout; // os dados do treino
   user = {} as User;
-  workoutModel = {} as Workout;
 
   form = new FormGroup({
     id: new FormControl(''),
@@ -23,8 +20,9 @@ export class AddSelectWorkoutComponent implements OnInit {
     description: new FormControl('', [
       Validators.required,
       Validators.minLength(4),
+      Validators.maxLength(100),
     ]),
-    datetime: new FormControl('', [Validators.required]),
+    datetime: new FormControl(this.getLocalDatetime(), [Validators.required]),
     exercises: new FormControl(
       [],
       [Validators.required, Validators.minLength(1)]
@@ -36,14 +34,103 @@ export class AddSelectWorkoutComponent implements OnInit {
     private utilsService: UtilsService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.user = this.utilsService.getElementFromLocalStorage('user');
+
+    if (this.workout) {
+      this.form.setValue(this.workout);
+      this.form.updateValueAndValidity();
+    }
+  }
+
+  getLocalDatetime() {
+    const now = new Date();
+    const timezoneOffset = now.getTimezoneOffset() * 60000;
+    const localISOTime = new Date(now.getTime() - timezoneOffset).toISOString().slice(0, -1);
+    return localISOTime;
+  }
 
   submit() {
-
+    if(this.form.valid) {
+      if (this.workout) {
+        this.updateWorkout();
+      } else {
+        this.createWorkout();
+      }
+    }
   }
 
   closeModal() {
     this.utilsService.dismissModal();
+  }
+
+  createWorkout() {
+    let path = `Users/${this.user.uid}`;
+    this.utilsService.presentLoading();
+
+    delete this.form.value.id;
+
+    this.firebaseService
+      .addToSubCollection(path, 'workouts', this.form.value)
+      .then(
+        (res) => {
+          this.utilsService.dismissModal({ success: true });
+
+          this.utilsService.presentToast({
+            message: 'Workout created successfully',
+            color: 'secondary',
+            icon: 'checkmark-circle-outline',
+            duration: 1500,
+          });
+          this.utilsService.dismissLoading();
+        },
+        (error) => {
+          this.utilsService.dismissModal({ success: true });
+
+          this.utilsService.presentToast({
+            message: error,
+            color: 'warning',
+            icon: 'alert-circle-outline',
+            duration: 5000,
+          });
+
+          this.utilsService.dismissLoading();
+        }
+      );
+  }
+
+  updateWorkout() {
+    let path = `Users/${this.user.uid}/workouts/${this.workout.id}`;
+
+    this.utilsService.presentLoading();
+    delete this.form.value.id;
+
+    this.firebaseService
+      .updateDocument(path, this.form.value)
+      .then(
+        (res) => {
+          this.utilsService.dismissModal({ success: true });
+
+          this.utilsService.presentToast({
+            message: 'Workout updated successfully',
+            color: 'secondary',
+            icon: 'checkmark-circle-outline',
+            duration: 1500
+          });
+          this.utilsService.dismissLoading();
+        },
+        (error) => {
+
+          this.utilsService.presentToast({
+            message: error,
+            color: 'warning',
+            icon: 'alert-circle-outline',
+            duration: 5000
+          });
+
+          this.utilsService.dismissLoading();
+        }
+      );
   }
 
   createItem() {
@@ -54,7 +141,7 @@ export class AddSelectWorkoutComponent implements OnInit {
         {
           name: 'name',
           type: 'textarea',
-          placeholder: 'Type something...',
+          placeholder: 'Type the exercise...',
         },
       ],
       buttons: [
@@ -65,9 +152,9 @@ export class AddSelectWorkoutComponent implements OnInit {
         {
           text: 'Add',
           handler: (res) => {
-            // let item: Item = { name: res.name, completed: false };
-            // this.form.value.items.push(item);
-            // this.form.controls.items.updateValueAndValidity();
+            let exercise: Exercise = { name: res.name, completed: false };
+            this.form.value.exercises.push(exercise);
+            this.form.controls.exercises.updateValueAndValidity();
           },
         },
       ],
