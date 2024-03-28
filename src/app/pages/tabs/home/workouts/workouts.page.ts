6a539e81ee1a3045eb62badee1ed1105fb08dc29
MarcusFurtuoso <marcusvini678@gmail.com>
2024-03-28 14:02:35 -0300

    feat: implements workout page

--- src/app/pages/tabs/home/workouts/workouts.page.ts
+++ src/app/pages/tabs/home/workouts/workouts.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import { Workout } from 'src/app/models/workout.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
@@ -10,28 +11,103 @@ import { AddSelectWorkoutComponent } from 'src/app/shared/components/add-select-
   styleUrls: ['./workouts.page.scss'],
 })
 export class WorkoutsPage implements OnInit {
+  user = {} as User;
+  workouts: Workout[] = [];
+  loading: boolean = false;
+
   constructor(
     private firebaseService: FirebaseService,
     private utilsService: UtilsService
   ) {}
 
-  flag: boolean = false;
-
   ngOnInit() {
-    this.creteTraining();
+    this.getUser();
+    this.getWorkouts();
   }
 
-  async creteTraining(workout?: Workout) {
+  getUser() {
+    return (this.user = this.utilsService.getElementFromLocalStorage('user'));
+  }
+
+  getWorkouts() {
+    let user: User = this.utilsService.getElementFromLocalStorage('user');
+    let path = `Users/${user.uid}`;
+
+    this.loading = true;
+
+    let sub = this.firebaseService
+      .getSubCollection(path, 'workouts')
+      .subscribe({
+        next: (res: Workout[]) => {
+          console.log(res);
+          this.workouts = res;
+          sub.unsubscribe();
+          this.loading = false;
+        },
+      });
+  }
+
+  async addOrSelectedWorkout(workout?: Workout) {
     let res = await this.utilsService.presentModal({
       component: AddSelectWorkoutComponent,
       componentProps: {
-        'isEditable': 'true',
+        workout,
       },
       cssClass: '.add-update-modal',
     });
 
-    // if (res && res.success) {
-    //   this.getTasks();
-    // }
+    if (res && res.success) {
+      this.getWorkouts();
+    }
+  }
+
+  confirmDeleteWorkout(workout: Workout) {
+    this.utilsService.presentAlert({
+      header: 'Delete workout',
+      message: 'Do you want to delete this workout?',
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Yes, delete',
+          handler: () => {
+            this.deleteWorkout(workout);
+          },
+        },
+      ],
+    });
+  }
+
+  deleteWorkout(workout: Workout) {
+    let path = `Users/${this.user.uid}/workouts/${workout.id}`;
+
+    this.utilsService.presentLoading();
+
+    this.firebaseService.deleteDocument(path).then(
+      (res) => {
+        this.utilsService.presentToast({
+          message: 'Workout deleted successfully',
+          color: 'secondary',
+          icon: 'checkmark-circle-outline',
+          duration: 1500,
+        });
+
+        this.getWorkouts();
+        this.utilsService.dismissLoading();
+      },
+      (error) => {
+        this.utilsService.presentToast({
+          message: 'Error when deleting workout!',
+          color: 'danger',
+          icon: 'alert-circle-outline',
+          duration: 5000,
+        });
+
+        this.utilsService.dismissLoading();
+      }
+    );
   }
 }
