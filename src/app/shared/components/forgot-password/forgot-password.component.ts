b70b506884bdc994a9555afc60fbd6cf206537b1
MarcusFurtuoso <marcusvini678@gmail.com>
2024-03-28 14:03:10 -0300

    feat: implemented forgot-password component

--- src/app/shared/components/forgot-password/forgot-password.component.ts
+++ src/app/shared/components/forgot-password/forgot-password.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,48 +10,42 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
-sendPasswordResetEmail() {
-throw new Error('Method not implemented.');
-}
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
   });
 
   constructor(
-    // private firebaseService: FirebaseService,
-    // private utilsService: UtilsService
+    private firebaseService: FirebaseService,
+    private utilsService: UtilsService
   ) {}
 
   ngOnInit() {}
 
-  // async sendPasswordResetEmail() {
-  //   if (this.form.valid) {
-  //     console.log(this.form.value.email);
-  //     await this.firebaseService
-  //       .sendPasswordResetEmail(this.form.value.email)
-  //       .then(
-  //         (res) => {
-  //           console.log(res);
-  //           console.log('Password reset email sent');
+  async sendPasswordResetEmail() {
+    if (this.form.valid) {
+      await this.firebaseService
+        .sendPasswordResetEmail(this.form.value.email)
+        .then(
+          (res) => {
+            console.log(res);
+            this.utilsService.presentToast({
+              message: `Password reset email sent!`,
+              duration: 1500,
+              color: 'secondary',
+              icon: 'mail-outline',
+            });
+          },
+          (error) => {
+            console.log(error);
 
-  //           this.utilsService.presentToast({
-  //             message: `Password reset email sent!`,
-  //             duration: 1500,
-  //             color: 'primary',
-  //             icon: 'mail-outline',
-  //           });
-  //         },
-  //         (error) => {
-  //           console.log(error);
-
-  //           this.utilsService.presentToast({
-  //             message: 'Error sending password reset email!',
-  //             duration: 5000,
-  //             color: 'danger',
-  //             icon: 'alert-circle-outline',
-  //           });
-  //         }
-  //       );
-  //   }
-  // }
+            this.utilsService.presentToast({
+              message: 'Error sending password reset email!',
+              duration: 5000,
+              color: 'danger',
+              icon: 'alert-circle-outline',
+            });
+          }
+        );
+    }
+  }
 }
