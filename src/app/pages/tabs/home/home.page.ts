2f8745ee251eb779366aaa6d0e4fe35a2a958283
MarcusFurtuoso <marcusvini678@gmail.com>
2024-03-28 13:59:27 -0300

    feat: add styles and implented user methods

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, filter, interval, take } from 'rxjs';
+import { User } from 'src/app/models/user.model';
 import { Workout } from 'src/app/models/workout.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
@@ -10,27 +12,49 @@ import { AddSelectWorkoutComponent } from 'src/app/shared/components/add-select-
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  user = {} as User;
+  workouts: Workout[] = [];
+  loading: boolean = false;
+  currentDate: string;
+
   constructor(
     private firebaseService: FirebaseService,
     private utilsService: UtilsService
-  ) {}
-
-  ngOnInit() {
-    // this.selectedWorkout();
+  ) {
+    const date = new Date();
+    this.currentDate = date.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
   }
 
+  ngOnInit() {
+
+  }
+
+  ionViewWillEnter() {
+    this.waitForUserToLoad(
+    ).subscribe(() => {
+      this.getUser();
+    });
+  }
+
+  getUser() {
+    return (this.user = this.utilsService.getElementFromLocalStorage('user'));
+  }
+
+  waitForUserToLoad(): Observable<number> {
+    return interval(100).pipe(
+      filter(() => !!localStorage.getItem('user')),
+      take(1)
+    );
+  }
 
   async selectedWorkout(workout?: Workout) {
-    let res = await this.utilsService.presentModal({
+    await this.utilsService.presentModal({
       component: AddSelectWorkoutComponent,
       componentProps: {
         workout,
       },
       cssClass: '.add-update-modal',
     });
-
-    // if (res && res.success) {
-    //   this.getTasks();
-    // }
   }
 }
