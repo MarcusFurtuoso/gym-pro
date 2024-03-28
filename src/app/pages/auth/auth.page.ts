fd427e8f67ab60f75dccbf7f512bb6b03062d262
MarcusFurtuoso <marcusvini678@gmail.com>
2024-03-28 13:58:01 -0300

    feat: implemented submit and form

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -5,6 +5,10 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { map, tap } from 'rxjs';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-auth',
@@ -12,35 +16,74 @@ import {
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-
-
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
 
-
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private firebaseService: FirebaseService,
+    private utilsService: UtilsService
+  ) {}
 
   ngOnInit() {
     null;
   }
 
-  submit() {
-    throw new Error('Method not implemented.');
+  loginWithGoogle() {
+    this.firebaseService.loginWithGoogle();
   }
 
-  loginWithGoogle() {
-    throw new Error('Method not implemented.');
+  submit(): void {
+    if (this.form.valid) {
+      this.utilsService.presentLoading({ message: 'Authenticating...' });
+
+      this.firebaseService.login(this.form.value as User).then(
+        async (res) => {
+          console.log(res);
+
+          this.firebaseService
+            .getUser(res.user.uid)
+            .pipe(
+              map((user) => {
+                return {
+                  ...user,
+                  uid: res.user.uid,  
+                  height: user.height,
+                  weight: user.weight,
+                };
+              }),
+              tap((user) => {
+                this.utilsService.setElementFromLocalStorage('user', user);
+              })
+            )
+            .subscribe();
+
+          this.utilsService.routerLink('/tabs/home');
+          this.utilsService.dismissLoading();
+
+          this.utilsService.presentToast({
+            message: `Login successfully!`,
+            duration: 1500,
+            color: 'secondary',
+            icon: 'person-outline',
+          });
+
+          this.form.reset();
+        },
+        (error) => {
+          this.utilsService.dismissLoading();
+
+          console.log(error);
+
+          this.utilsService.presentToast({
+            message: 'Error when authenticating!',
+            duration: 5000,
+            color: 'danger',
+            icon: 'alert-circle-outline',
+          });
+        }
+      );
     }
-    
-  // createLoginForm() {
-  //   this.loginForm = this.formBuilder.group({
-  //     email: new FormControl('', [Validators.required, Validators.email]),
-  //     password: new FormControl('', [
-  //       Validators.required,
-  //       Validators.minLength(6),
-  //     ]),
-  //   });
-  // }
+  }
 }
--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
+import { CustomValidators } from 'src/app/utils/custom-validators';
 
 @Component({
   selector: 'app-register',
@@ -9,21 +14,78 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class RegisterPage implements OnInit {
 
   form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
     name: new FormControl('', [Validators.required]),
-    sexo: new FormControl('', [Validators.required]),
+    height: new FormControl(''),
+    weight: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
     confirmPassword: new FormControl(''),
   });
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private utilsService: UtilsService
+  ) {}
 
   ngOnInit() {
+    this.confirmPasswordValidator();
   }
 
+  confirmPasswordValidator() {
+    this.form.controls.confirmPassword.setValidators([
+      Validators.required,
+      CustomValidators.matchValues(this.form.controls.password),
+    ]);
 
-  submit() {
-    throw new Error('Method not implemented.');
+    this.form.controls.confirmPassword.updateValueAndValidity();
   }
 
+  submit(): void {
+    if (this.form.valid) {
+      this.utilsService.presentLoading({ message: 'Registering...' });
+
+      let { confirmPassword, ...userWithoutConfirmPassword } = this.form.value;
+
+      this.firebaseService.register(userWithoutConfirmPassword as User).then(
+        async (res) => {
+          console.log(res);
+
+          await this.firebaseService.updateUser({
+            displayName: this.form.value.name,
+          });
+
+          let user: User = {
+            uid: res.user.uid,
+            name: res.user.displayName,
+            email: res.user.email,
+            height: null,
+            weight: null,
+          };
+
+          this.utilsService.setElementFromLocalStorage('user', user);
+          this.utilsService.routerLink('/tabs/home');
+          this.utilsService.dismissLoading();
+
+          this.utilsService.presentToast({
+            message: `Registration successful ${user.name}!`,
+            duration: 1500,
+            color: 'secondary',
+            icon: 'person-outline',
+          });
+
+          this.form.reset();
+        },
+        (error) => {
+          this.utilsService.dismissLoading();
+          console.error(error);
+          this.utilsService.presentToast({
+            message: 'Registration failed!',
+            duration: 5000,
+            color: 'danger',
+            icon: 'alert-circle-outline',
+          });
+        }
+      );
+    }
+  }
 }
