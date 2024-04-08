4341e56de76de4be5b1611256f56bda47d09a8f0
MarcusFurtuoso <marcusvini678@gmail.com>
2024-04-08 20:38:47 -0300

    feat: add button when modal

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 // import { ThemeService } from 'src/app/services/theme.service';
-// import { UtilsService } from 'src/app/services/utils.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
-
+export class HeaderComponent implements OnInit {
   @Input() title: string;
   @Input() titleButton: string;
   @Input() backButton: string;
@@ -17,22 +16,17 @@ export class HeaderComponent  implements OnInit {
   @Input() color: string;
   @Input() centerTitle: boolean;
 
-
   // darkMode: BehaviorSubject<boolean>;
 
-  // constructor(private themeService: ThemeService,
-  //   private utilsService: UtilsService) { }
+  constructor(private utilsService: UtilsService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
+  dimissModal() {
+    this.utilsService.dismissModal();
   }
-
-  // dimissModal() {
-  //   this.utilsService.dismissModal();
-  // }
 
   // setTheme(darkmode: boolean) {
   //   this.themeService.setTheme(darkmode);
   // }
-
 }
